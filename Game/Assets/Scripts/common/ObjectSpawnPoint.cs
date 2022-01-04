2e23fd06c4d7181e7de6b873d0e59dee018acd7e
tuyuer <wowo5050@126.com>
2022-01-04 09:35:14 +0800

    shooter

---
+++ Game/Assets/Scripts/common/ObjectSpawnPoint.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnPoint : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public int spawnTimes = 10;
+    public float spawnColdTime = 2.0f;
+    private float spawnElapsedTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnElapsedTime = spawnColdTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnElapsedTime += Time.deltaTime;
+        if (spawnElapsedTime > spawnColdTime)
+        {
+            SpawnObject();
+            spawnElapsedTime = 0f;
+        }
+    }
+
+    private void SpawnObject()
+    {
+        if (objectToSpawn != null)
+        {
+            GameObject clonedObj = Instantiate(objectToSpawn);
+            clonedObj.transform.parent = transform;
+            clonedObj.transform.position = transform.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color originColor = Gizmos.color;
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+        Gizmos.color = originColor;
+    }
+}
