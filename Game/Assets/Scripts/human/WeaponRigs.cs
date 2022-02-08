8dfdaffcc00661ab0340b0b1971b053589d48a65
tuyuer <wowo5050@126.com>
2022-02-08 23:17:43 +0800

    update

---
+++ Game/Assets/Scripts/human/WeaponRigs.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class WeaponRigs : MonoBehaviour
+{
+    public Transform lh_ik;
+    public Transform rh_ik;
+
+    public Vector3 pistol_lh = new Vector3(0.08f, 1.47f, 0.365f);
+    public Vector3 pistol_rh = new Vector3(0.2f, 1.5f, 0.3f);
+
+    public Vector3 rifle_lh = new Vector3(0.08f, 1.47f, 0.365f);
+    public Vector3 rifle_rh = new Vector3(0.2f, 1.5f, 0.3f);
+
+    private Vector3 lh_pos;
+    private Vector3 rh_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WeaponPistol();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void WeaponPistol()
+    {
+        lh_pos = pistol_lh;
+        rh_pos = pistol_rh;
+
+        lh_ik.position = lh_pos;
+        rh_ik.position = rh_pos;
+    }
+
+    public void WeaponRifle()
+    {
+        
+    }
+}
